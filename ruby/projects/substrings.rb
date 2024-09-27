c53fd96040ba41f4850603a7c31f42f074f341f3
tokisuno <lucaswmei@gmail.com>
2024-09-27 14:05:25 -0400

    done substrings

--- ruby/projects/substrings.rb
+++ ruby/projects/substrings.rb
@@ -4,13 +4,13 @@ test_input = "Howdy partner, sit down! How's it going?"
 def substrings(input, dict)
   string = input.downcase.split(/\W/)
 
-  data = Array.new() { Hash.new }
-
+  hash = {}
   string.each do |word|
     if dict.include?(word)
-      puts word
+      if hash.include?(word) then hash[word] += 1 else hash[word] = 1 end
     end
   end
+  puts hash
 end
 
 substrings(test_input, dictionary)
