2a458cdeab8d46cafef3a75555e435cf45f27b93
tokisuno <lucaswmei@gmail.com>
2025-01-31 16:26:01 -0500

    colours are almost there, win condition is recognised

--- ruby/projects/mastermind/lib/game.rb
+++ ruby/projects/mastermind/lib/game.rb
@@ -6,6 +6,17 @@ require 'colorize'
 
 # NOTE: Maybe make a method that does a pretty-print of the table?
 
+SQUARE = '■'.freeze
+
+GAME_COLOURS = {
+  1 => SQUARE.colorize(:blue),
+  2 => SQUARE.colorize(:green),
+  3 => SQUARE.colorize(:red),
+  4 => SQUARE.colorize(:yellow),
+  5 => SQUARE.colorize(:brown),
+  6 => SQUARE.colorize(:light_black)
+}.freeze
+
 # Check if input is valid
 # TODO: Ensure that characters don't get by
 module InputValidation
@@ -13,7 +24,11 @@ module InputValidation
     tmp_array = []
     arr.each_with_index do |_, i|
       tmp = Integer(arr[i], exception: false)
-      return false if tmp > 6 || tmp < 1
+
+      if tmp > 6 || tmp < 1
+        return false
+      end
+
       tmp_array.push(tmp)
     end
     @player.guess = tmp_array
@@ -23,27 +38,20 @@ end
 
 # Checks result of answer
 module CheckResults
-  CORRECT = '▣'.freeze
-  CLOSE   = '▧'.freeze
-  # def check_results
-  #   p @player.guess
-  #   p @cpu.secret_code
-  #   @cpu.secret_code.each_with_index do |_, i|
-  #     if @cpu.secret_code[i] == @player.guess[i]
-  #       @guesses[i] = CORRECT
-  #       next
-  #     end
-  #
-  #     if @cpu.secret_code[i] != CORRECT
-  #       if @cpu.secret_code.include? @player.guess[i]
-  #         @guesses[@player.guess[@player.guess.index(@player.guess[i])]] = CLOSE
-  #         next
-  #       end
-  #     end
-  #   end
-  #   print @guesses
-  #   @guesses = ['□', '□', '□', '□']
-  # end
+  def check_results
+    @player.guess.each_with_index do |_, i|
+      if @player.guess[i] == @cpu.secret_code[i]
+        @result.push 100
+        puts 'same spot'
+        next
+      end
+      if @cpu.secret_code.include?(@player.guess[i])
+        @result.push 2
+        puts 'in the array'
+        next
+      end
+    end
+  end
 end
 
 # Game class
@@ -56,24 +64,28 @@ class Game
   def initialize
     @player = 0
     @cpu = CPU.new
-    # @guesses = ['□', '□', '□', '□']
+    @result = []
     @game = true
     @turns = 0
   end
+
 
   def play
     puts 'What is your name, challenger?'.colorize(:red)
     @player = Player.new(gets.chomp)
 
+    display_colour_blocks
+
     puts @cpu.secret_code
 
     while @game == true
       guess
-      # check_results
-      # @turns += 1
-      # puts @turns
+      check_results
+      display_results
     end
   end
+
+  private
 
   def guess
     while 1
@@ -87,6 +99,26 @@ class Game
       end
 
       puts 'OOPS, invalid input :/'
+    end
+  end
+
+  def display_results
+    @result.each do |item|
+      print SQUARE.colorize(:red) if item == 100
+      print SQUARE.colorize(:grey) if item == 2
+    end
+    puts ''
+    if @result.sum == 400
+      puts 'you win!'
+      @game = false
+    end
+    @result = []
+  end
+
+
+  def display_colour_blocks
+    (1..6).each do |i|
+      puts "#{i}: #{GAME_COLOURS[i]}"
     end
   end
 end
