4124dd23dc3e0ae153576175c0b9acc51295dcaa
tokisuno <lucaswmei@gmail.com>
2025-02-18 08:57:22 -0500

    starting pattern matching "forgot to push this oops"

---
+++ ruby/pattern-matching/return-values.rb
@@ -0,0 +1,10 @@
+grade = 'C'
+
+result = case grade
+         in 'A' then 1
+         in 'B' then 2
+         in 'C' then 3
+         else 0
+         end
+
+puts result
---
+++ ruby/pattern-matching/type-comparisons.rb
@@ -0,0 +1,6 @@
+input = 3
+
+case input
+in String then puts 'Input was of type String'
+in Integer then puts 'Input was of type Integer'
+end
